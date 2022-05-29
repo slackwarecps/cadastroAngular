679a2029e01b2bebc00ea47d7fc6edf8165a2b67
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-29 09:06:23 -0300

    Servico Pessoa Adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
-import { RodapeModule } from './componentes/rodape/rodape.module';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    MatSliderModule,
     BrowserModule,
     AppRoutingModule,
-    CabecalhoModule,
-    RodapeModule,
     BrowserAnimationsModule,
-    MatButtonModule,
     MatToolbarModule,
-    MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pessoa/pessoa.component.ts
+++ src/app/pessoa/pessoa.component.ts
@@ -1,5 +1,7 @@
-import { Pessoa } from './../model/pessoa';
 import { Component, OnInit } from '@angular/core';
+
+import { Pessoa } from './../model/pessoa';
+import { PessoasService } from './services/pessoas.service';
 
 @Component({
   selector: 'app-pessoa',
@@ -7,15 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pessoa.component.css'],
 })
 export class PessoaComponent implements OnInit {
-  public pessoas: Pessoa[] = [
+  public pessoas: Pessoa[] = [];
+
+  public pessoas2: Pessoa[] = [
     { id: 1, nome: 'Fabio Alvaro Pereira', idade: 43 },
     { id: 2, nome: 'Tatiana Vercelino Favoretti', idade: 43 },
     { id: 3, nome: 'Isabela Pereira', idade: 17 },
   ];
   displayedColumns = ['nome', 'idade'];
 
-  constructor() {
+  constructor(private pessoasService: PessoasService) {
     //this.pessoas = [];
+    // this.pessoasService = new PessoasService();
+    this.pessoas = pessoasService.list();
   }
 
   ngOnInit(): void {}
